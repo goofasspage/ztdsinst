7c018828df810b51e9c38cec5d1ad923cbc9586b
Goofasspage 2 <30077553+goofasspage@users.noreply.github.com>
2019-07-27 12:59:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -130,7 +130,7 @@ mkdir -m 777 /var/www/html/$domain
 
 rm -rf /tmp/ztds
 rm -f /tmp/ztds.7z
-curl -L -o /tmp/ztds.7z https://github.com/spartanetsru/ztds_installer/blob/master/$ztds_version.7z?raw=true
+curl -L -o /tmp/ztds.7z https://github.com/goofasspage/ztdsinst/blob/master/$ztds_version.7z?raw=true
 7za x -o/tmp/ztds /tmp/ztds.7z
 cp -a /tmp/ztds/$ztds_version/. /var/www/html/$domain
 chmod 777 -R /var/www/html/$domain
